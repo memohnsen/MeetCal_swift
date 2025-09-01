e1b59b01c26cc9e3c49a5f28166a887e3a832e84
Maddisen Mohnsen <memohnsen@gmail.com>
2025-09-01 16:54:09 -0400

    Initial Commit

---
+++ meetcal/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  meetcal
+//
+//  Created by Maddisen Mohnsen on 9/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ meetcalTests/meetcalTests.swift
@@ -0,0 +1,17 @@
+//
+//  meetcalTests.swift
+//  meetcalTests
+//
+//  Created by Maddisen Mohnsen on 9/1/25.
+//
+
+import Testing
+@testable import meetcal
+
+struct meetcalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
